8ad95d40873949091463dfb07aaf85432d38b69b
Hail Mary <41487770+alejosjen@users.noreply.github.com>
2018-12-22 21:33:37 -0700

    update Letter.js and Word.js with constructor functions. Figuring out index.js prompts using inquirer

--- word.js
+++ word.js
@@ -1,15 +1,31 @@
-var LETTER = require("./Letter.js");
+const Letter = require("./Letter");
 
-var WORD = function () {
-    var currentWord = new LETTER([]);
-    currentWord.returnUnderscoreOrLetter().join(" ");
-    currentWord.checkLetters(letter);
-};
+var Word = function(word){
+    console.log("From Word.js " + word)
+    //Make an array
+    const stringifiedLetters = word.split("");
+    //Map is an empty array, it calls function and pushes the return to itself as a callback
+    this.letters = stringifiedLetters.map(function(character){
+       //needs return, the new Letter is the callback function passing in the character
+        return new Letter(character)
+    });
+}
+Word.prototype.toString = function (){
+        //.join will automatically loop and bring things together
+        //toString prints as a string 
+        return this.letters.join(" ");
+    }
 
+//Takes the argument and calls second function for the guess function on each letter
+Word.prototype.makeGuess = function(character){
+    var foundLetter = false;
+    this.letters.forEach(function(character) {
+        if (this.letters.isGuessed(character)) {
+            foundLetter = true;
+        }
+    })
+    console.log("\n" + this + "\n");
+    return foundLetter;
+}
 
-
-    wordBank = wordsList[Math.floor(Math.random() * wordsList.length)];
-
-
-
-module.exports = WORD;
+module.exports = Word;
